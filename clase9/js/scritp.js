39f21bcf0bd86f985bab4bbbf422770580cb3984
manolo <gersonquiroz66@gmail.com>
2023-10-19 19:12:33 -0300

    fin de clase 9

--- clase9/js/scritp.js
+++ clase9/js/scritp.js
@@ -1,1 +1,19 @@
-// hola
+let edad = parseFloat(prompt("ingrese su edad"))
+let altura = parseFloat(prompt("ingresa tu altura en cm"))
+
+
+if (edad >=18 && altura >=150){
+   // si la condicion es vervadera
+   // se ejecuta todo o que yo defina aca
+   alert("podes entrar a la pagina")
+} else if (altura <150){
+   // se ejecuta todo o que yo defina aca SOLO SI LA CONDICION ES FALSA
+   alert("sos muy baj@, tomate el palo de aca")
+}else{
+   alert("tomate el palo de aca")
+}
+
+
+let listFrutas = ["banana", "naranja", "pomelo","papaya", "vamo boca", 7]
+
+console.log(listFrutas[5])
